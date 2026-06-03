cb0978b0ff2456acc7abcbd9f0056786a44dfcec
Rakesh172-1 <sonicstory217@gmail.com>
2026-06-03 23:44:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Vriddhi',
+      title: 'Agronatural',
       theme: AppTheme.lightTheme,
       routerConfig: _buildRouter(),
     );
